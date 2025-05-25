b78a639e79ca35828bb71d23e817142cf7ad7f5b
yinvestor <bigirwarft76@gmail.com>
2025-05-25 17:46:29 +0300

    Fine tune 1

--- finalYearProject/HealthAI/urls.py
+++ finalYearProject/HealthAI/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path("index", views.index, name="index"),
     path("checkup", views.checkup, name="checkup"),
     path("login", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout", auth_views.LogoutView.as_view(template_name="index.html"), name="logout"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
