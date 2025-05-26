6e257b6e99a3415f17f64866bcc7cad0dee5928c
yinvestor <bigirwarft76@gmail.com>
2025-05-26 14:27:01 +0300

    Fine tune 2

--- finalYearProject/HealthAI/urls.py
+++ finalYearProject/HealthAI/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -8,13 +8,24 @@ urlpatterns = [
     path("register", views.register, name="register"),
     # path("wewandiise", views.register_user, name="wewandiise"),
     path("prediction", views.prediction, name="prediction"),
-    path("index", views.index, name="index"),
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
     path("checkup", views.checkup, name="checkup"),
     path("login", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout", auth_views.LogoutView.as_view(template_name="index.html"), name="logout"),
+    # path("logout/", auth_views.LogoutView.as_view(next_page="index"), name="logout"),
+    path("logout/", views.logout_view, name="logout"),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='auth/password_reset_confirm.html',
+             success_url=reverse_lazy('login')
+         ),
+         name='password_reset_confirm'),
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='auth/password_reset_complete.html'
+         ), 
+         name='password_reset_complete'),
 
 ]
--- finalYearProject/HealthAI/views.py
+++ finalYearProject/HealthAI/views.py
@@ -8,14 +8,104 @@ import joblib
 import numpy as np
 from django.contrib.auth.models import User
 
-model = joblib.load('c:/Users/User/Documents/Health-AI/finalYearProject/project-models/model_training/heart_disease_xgboost_model.pkl')
-scaler = joblib.load('c:/Users/User/Documents/Health-AI/finalYearProject/project-models/model_training/heart_disease_scaler.pkl')
+model = joblib.load('c:/Users/Administrator/.vscode/healthai/Health-AI/finalYearProject/project-models/model_training/heart_disease_xgboost_model.pkl')
+scaler = joblib.load('c:/Users/Administrator/.vscode/healthai/Health-AI/finalYearProject/project-models/model_training/heart_disease_scaler.pkl')
 
 def base(request):
     return render(request, 'base.html')
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    parameters = [
+    {
+        "title": "CP (Chest Pain Type)",
+        "description": "Type of chest pain experienced.",
+        "values": [
+            "0: Typical angina",
+            "1: Atypical angina",
+            "2: Non-anginal pain",
+            "3: Asymptomatic"
+        ],
+        "note": "Chest pain type helps assess heart risk and whether pain is heart-related."
+    },
+    {
+        "title": "Trestbps (Resting Blood Pressure)",
+        "description": "Resting blood pressure in mm Hg at admission to the hospital.",
+        "note": "A higher value may indicate hypertension and increase heart disease risk."
+    },
+    {
+        "title": "Chol (Serum Cholesterol)",
+        "description": "Serum cholesterol in mg/dl.",
+        "note": "Total cholesterol includes LDL, HDL, and triglycerides. High levels can block arteries."
+    },
+    {
+        "title": "FBS (Fasting Blood Sugar)",
+        "description": "Fasting blood sugar > 120 mg/dl.",
+        "values": [
+            "1: True (high)",
+            "0: False (normal)"
+        ],
+        "note": "High fasting blood sugar may indicate diabetes, a major heart disease risk factor."
+    },
+    {
+        "title": "Restecg (Resting Electrocardiographic Results)",
+        "description": "ECG results at rest.",
+        "values": [
+            "0: Normal",
+            "1: Having ST-T wave abnormality",
+            "2: Showing left ventricular hypertrophy"
+        ],
+        "note": "Used to identify irregular heartbeats, enlarged heart, or ischemia."
+    },
+    {
+        "title": "Thalach (Maximum Heart Rate Achieved)",
+        "description": "The highest heart  rate achieved during a stress test.",
+        "note": "Low values may indicate poor heart performance."
+    },
+    {
+        "title": "Exang (Exercise-Induced Angina)",
+        "description": "Whether chest pain occurred during exercise.",
+        "values": [
+            "1: Yes",
+            "0: No"
+        ],
+        "note": "Presence of angina during exercise may suggest blocked arteries."
+    },
+    {
+        "title": "Oldpeak (ST Depression Induced by Exercise Relative to Rest)",
+        "description": "Amount of ST depression observed during a stress test compared to rest.",
+        "note": "Higher values suggest the heart isn’t getting enough blood during activity."
+    },
+    {
+        "title": "Slope (Slope of the Peak Exercise ST Segment)",
+        "description": "Slope of the ST segment during peak exercise.",
+        "values": [
+            "0: Upsloping",
+            "1: Flat",
+            "2: Downsloping"
+        ],
+        "note": "Flat or downsloping ST segments often indicate heart issues."
+    },
+    {
+        "title": "CA (Number of Major Vessels Colored by Fluoroscopy)",
+        "description": "Number of major vessels (0–3) showing blood flow in a dye test.",
+        "note": "More blocked vessels means higher chance of heart disease."
+    },
+    {
+        "title": "Thal (Thallium Stress Test Result)",
+        "description": "Results of a nuclear imaging test for blood flow in the heart.",
+        "values": [
+            "1: Normal",
+            "2: Fixed defect (previous myocardial infarction)",
+            "3: Reversible defect (possible ischemia)"
+        ],
+        "note": "Used to determine areas of reduced blood flow in the heart."
+    }
+]
+
+    return render(request, 'about.html', {"parameters": parameters})
 
 def register(request):
     if request.method == 'POST':
@@ -79,8 +169,8 @@ def login_view(request):
     return render(request, 'login.html', {'form': form})
 
 def logout_view(request):
-    if request.method == "POST":
-        logout(request)
-        return redirect('index')
-    else:
-        return redirect('index')
+    # if request.method == "POST":
+    logout(request)
+    return redirect('index')
+    # else:
+    #     return redirect('index')
