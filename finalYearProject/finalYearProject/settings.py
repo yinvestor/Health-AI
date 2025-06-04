f7528d12f45c187fcb60906ee392ed2602119836
Bigirwa Arafat <161699072+yinvestor@users.noreply.github.com>
2025-06-04 07:38:46 +0300

    Update settings.py

--- finalYearProject/finalYearProject/settings.py
+++ finalYearProject/finalYearProject/settings.py
@@ -94,10 +94,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'bigirwarft76@gmail.com'
-# EMAIL_HOST_PASSWORD = 'gifblyeirabewbbt'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
 # EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'bigirwarft76@gmail.com'
+# DEFAULT_FROM_EMAIL = ''
 
 
 AUTHENTICATION_BACKENDS = [
